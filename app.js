2cfbc51c690ada3ce9843396c7457aaef5672a88
Gil Woo Jin <rlftnwls0101@gmail.com>
2025-10-01 14:40:44 +0900

    update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/member',membarRouter.router);
 
 // 404not Found 
 app.use((req,res)=>{
-    res.status(404).send('404오류다. 끄지라')
+    res.status(404).send('404오류다. 잘못된접근입니다.')
 });
 
 app.listen(
